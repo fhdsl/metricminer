93846ba0a8eb8936795b406a186cde0af65ee766
seankross <smk240@gmail.com>
2023-06-02 16:22:17 -0700

    ga app first draft complete

--- app.R
+++ app.R
@@ -2,6 +2,11 @@ library(shiny)
 library(tidyverse)
 library(here)
 library(ggplot2)
+library(slider)
+
+#################
+## Data Import ##
+#################
 
 ga_data_ <- here("data", "ga.csv") |> read_csv() |>
   mutate(pagePath = gsub("//", "/", pagePath)) |>
@@ -13,19 +18,9 @@ ga_data_ <- here("data", "ga.csv") |> read_csv() |>
   mutate(activeUsers = sum(activeUsers), sessions = sum(sessions)) |>
   ungroup() |> unique()
 
-ga_ui_bdsw <- tabPanel(title = "other",
-                         fluidPage(
-                           sidebarLayout(
-                             sidebarPanel(
-                               selectInput("status", label = "Status of Purchase:",
-                                           choices = c("Purchased", "Cancelled")),
-                             ),
-                             mainPanel(
-                               # tableOutput('ga_tbl')
-                             )
-                           )
-                         )
-)
+######################
+## UI Specification ##
+######################
 
 ga_ui <- tabPanel(title = "Google Analytics",
                   fluidPage(
@@ -35,55 +30,187 @@ ga_ui <- tabPanel(title = "Google Analytics",
                         selectInput("ga_property", label = "Property",
                                     choices = NULL),
                         selectInput("ga_page", label = "Page",
-                                  choices = NULL),
+                                    choices = NULL),
+                        dateRangeInput("ga_date", label = "Date Range",
+                                       start = ga_data_$date |> as.Date() |> min(),
+                                       end = ga_data_$date |> as.Date() |> max())
                       ),
                       mainPanel(
                         tabsetPanel(type = "tabs",
-                          tabPanel("Plot", plotOutput("ga_plot")),
-                          tabPanel("Table", tableOutput('ga_tbl')),
+                                    tabPanel("Plot", br(), plotOutput("ga_dplot"), plotOutput("ga_wplot")),
+                                    tabPanel("Table", tableOutput('ga_tbl')),
+                                    tabPanel("Summary", tableOutput('ga_summary')),
                         )
                       )
                     )
                   )
 )
 
-ga_ui <- navbarMenu(title = "Dashboards",
-                    ga_ui, ga_ui_bdsw)
+slack_ui <- tabPanel(title = "FH Data Slack",
+                         fluidPage(
+                           h2("FH Data Slack"),
+                           sidebarLayout(
+                             sidebarPanel(
+                               p("Under Construction")
+                             ),
+                             mainPanel(
+                               p("Under Construction")
+                             )
+                           )
+                         )
+)
+
+calendly_ui <- tabPanel(title = "Calendly",
+                        fluidPage(
+                          h2("Calendly"),
+                          sidebarLayout(
+                            sidebarPanel(
+                              p("Under Construction")
+                            ),
+                            mainPanel(
+                              p("Under Construction")
+                            )
+                          )
+                        )
+)
+
+notion_ui <- tabPanel(title = "Notion",
+                        fluidPage(
+                          h2("Notion"),
+                          sidebarLayout(
+                            sidebarPanel(
+                              p("Under Construction")
+                            ),
+                            mainPanel(
+                              p("Under Construction")
+                            )
+                          )
+                        )
+)
+
+substack_ui <- tabPanel(title = "Substack",
+                      fluidPage(
+                        h2("Substack"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            p("Under Construction")
+                          ),
+                          mainPanel(
+                            p("Under Construction")
+                          )
+                        )
+                      )
+)
+
+dashboards_ui <- navbarMenu(title = "Dashboards",
+                            ga_ui,
+                            slack_ui,
+                            calendly_ui,
+                            notion_ui,
+                            substack_ui,
+                            )
 
 ui <- navbarPage(title = "DaSL Analytics",
-                 ga_ui)
+                 dashboards_ui)
+
+##########################
+## Server Specification ##
+##########################
 
 server <- function(input, output, session) {
+  # Dynamically populates Property input from data
   updateSelectInput(session = session, inputId = "ga_property",
                     choices = ga_data_ |> pull(Property) |> unique())
 
+  # Dynamically populates Page input based on selected Property
   observeEvent(input$ga_property,
                updateSelectInput(session = session, inputId = "ga_page",
                                  choices = ga_data_ |>
                                    filter(Property == input$ga_property) |>
                                    pull(pagePath) |> unique() |> sort()))
 
-  observeEvent(input$ga_page,
+  # Dynamically creates plot for daily metrics based on Property, Page, and
+  # Date Range
+  ga_daily_plot <- reactive({
+    ga_data_ |>
+      filter(Property == input$ga_property) |>
+      filter(pagePath == input$ga_page) |>
+      select(date, activeUsers, sessions) |>
+      mutate(date = as.Date(date)) |>
+      filter(date >= input$ga_date[1], date <= input$ga_date[2]) |>
+      rename(Date = date, "Active Users" = activeUsers, Sessions = sessions) |>
+      pivot_longer(cols = c("Active Users", "Sessions"),
+                   names_to = "Metric", values_to = "Value") |>
+      ggplot(aes(Date, Value, colour = Metric)) +
+      geom_line() +
+      ggtitle(paste("Daily Google Analytics for", input$ga_property, input$ga_page))
+  })
+
+  observeEvent(ga_daily_plot(),
+               output$ga_dplot <- renderPlot(
+                 ga_daily_plot()
+               ))
+
+  # Dynamically creates plot for weekly metrics based on Property, Page, and
+  # Date Range
+  ga_weekly_plot <- reactive({
+    ga_data_ |>
+      filter(Property == input$ga_property) |>
+      filter(pagePath == input$ga_page) |>
+      select(date, activeUsers, sessions) |>
+      mutate(date = as.Date(date)) |>
+      filter(date >= input$ga_date[1], date <= input$ga_date[2]) |>
+      arrange(date) |>
+      mutate(activeUsers = slide_dbl(activeUsers, mean, .before = 30)) |>
+      mutate(sessions = slide_dbl(sessions, mean, .before = 30)) |>
+      rename(Date = date, "Active Users" = activeUsers, Sessions = sessions) |>
+      pivot_longer(cols = c("Active Users", "Sessions"),
+                   names_to = "Metric", values_to = "Value") |>
+      ggplot(aes(Date, Value, colour = Metric)) +
+      geom_line() +
+      ggtitle(paste("Average Monthly Google Analytics for", input$ga_property, input$ga_page))
+  })
+
+  observeEvent(ga_weekly_plot(),
+               output$ga_wplot <- renderPlot(
+                 ga_weekly_plot()
+               ))
+
+  # Dynamically creates table for daily metrics based on Property, Page, and
+  # Date Range
+  ga_daily_table <- reactive({
+    ga_data_ |>
+      filter(Property == input$ga_property) |>
+      filter(pagePath == input$ga_page) |>
+      filter(date >= input$ga_date[1], date <= input$ga_date[2]) |>
+      select(date, activeUsers, sessions) |>
+      arrange(desc(date)) |>
+      rename(Date = date, "Active Users" = activeUsers, Sessions = sessions)
+  })
+
+  observeEvent(ga_daily_table(),
             output$ga_tbl <- renderTable(
-              ga_data_ |>
-                filter(Property == input$ga_property) |>
-                filter(pagePath == input$ga_page) |>
-                select(date, activeUsers, sessions) |>
-                arrange(desc(date))
+              ga_daily_table()
             ))
 
-  observeEvent(input$ga_page,
-               output$ga_plot <- renderPlot(
-                 ga_data_ |>
-                   filter(Property == input$ga_property) |>
-                   filter(pagePath == input$ga_page) |>
-                   select(date, activeUsers, sessions) |>
-                   mutate(date = as.Date(date)) |>
-                   rename(Date = date) |>
-                   pivot_longer(cols = c("activeUsers", "sessions"),
-                                names_to = "Metric", values_to = "Value") |>
-                   ggplot(aes(Date, Value, colour = Metric)) +
-                    geom_line()
+  # Dynamically creates table for most popular Pages based on Property and
+  # Date Range
+  ga_summary <- reactive({
+    ga_data_ |>
+      mutate(date = as.Date(date)) |>
+      filter(Property == input$ga_property) |>
+      filter(date >= input$ga_date[1], date <= input$ga_date[2]) |>
+      group_by(pagePath) |>
+      summarize("Total Active Users" = sum(activeUsers),
+                "Total Sessions" = sum(sessions)) |>
+      ungroup() |>
+      arrange(desc(`Total Active Users`)) |>
+      rename(Page = pagePath)
+  })
+
+  observeEvent(ga_summary(),
+               output$ga_summary <- renderTable(
+                 ga_summary()
                ))
 }
 
