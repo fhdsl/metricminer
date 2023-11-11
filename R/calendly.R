ac2318f2b83d7944ab205b7b48601a545aba15ea
Candace Savonen <cansav09@gmail.com>
2023-11-10 21:21:11 -0500

    Calendly events retrieved!

--- R/calendly.R
+++ R/calendly.R
@@ -2,7 +2,10 @@
 
 #' Handle Calendly GET requests
 #' @description This is a function to get the Calendly API user info
-#' @param api_key You can provide the API key directly or this function will attempt to grab an API key that was stored using the `authorize("calendly")` function
+#' @param token You can provide the API key directly or this function will attempt to grab an API key that was stored using the `authorize("calendly")` function
+#' @param user The user param for Calendly. Usually looks like "https://api.calendly.com/users/c208a750-9214-4c62-9ee6-a1a9507c7b43"
+#' @param count For paginated GETs, you can specify how many things you'd like returned
+#' @param page_token For a paginated GET, what page are we on?
 #' @return Calendly REST API response as a list
 #' @importFrom utils menu installed.packages
 #' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
@@ -10,12 +13,16 @@
 #' @examples \dontrun{
 #'
 #' authorize("calendly")
-#' get_calendly_user()
+#' token <- get_token(app_name = "calendly")
+#'
+#' result_list <- calendly_get(
+#'   url = "https://api.calendly.com/users/me",
+#'   token = token
+#' )
 #' }
 #'
 # Get Calendly stuffs
-calendly_get <- function(url, token, user, count, page_token = NULL) {
-
+calendly_get <- function(url, token, user = NULL, count = NULL, page_token = NULL) {
   result <- httr::GET(
     url,
     query = list(
@@ -49,26 +56,19 @@ calendly_get <- function(url, token, user, count, page_token = NULL) {
 #' get_calendly_user()
 #' }
 get_calendly_user <- function(api_key) {
-  # Get auth token
-  token <- get_token(app_name = "calendly")
 
+  if (is.null(api_key)) {
+    # Get auth token
+    token <- get_token(app_name = "calendly")
+  } else {
+    token <- api_key
+  }
   # Declare URL
-  url <- "https://api.calendly.com/users/me"
-
-  # Github api get
-  result <- httr::GET(
-    url,
-    httr::add_headers(Authorization = paste0("Bearer ", token)),
-    httr::accept_json()
+  result_list <- calendly_get(
+    url = "https://api.calendly.com/users/me",
+    token = token
   )
 
-  if (httr::status_code(result) != 200) {
-    httr::stop_for_status(result)
-  }
-
-  # Process and return results
-  result_content <- httr::content(result, "text")
-  result_list <- jsonlite::fromJSON(result_content)
   return(result_list)
 }
 
@@ -87,9 +87,7 @@ get_calendly_user <- function(api_key) {
 #' list_calendly_events(user = user$resource$uri)
 #' }
 #'
-#' user <- "https://api.calendly.com/users/c208a750-9214-4c62-9ee6-a1a9507c7b43"
 list_calendly_events <- function(user, count = 100) {
-
   token <- get_token(app_name = "calendly")
 
   # Only can handle requests with 100 at a time
@@ -104,29 +102,26 @@ list_calendly_events <- function(user, count = 100) {
   )
 
   if (count > result_list$pagination$count) {
-
     # Set up a while loop for us to store the multiple page requests in
     cummulative_pages <- list()
     cummulative_pages[[1]] <- result_list$collection
     page <- 1
 
     while (!is.null(result_list$pagination$next_page_token)) {
-
-    result_list <-
-      calendly_get(
-      url = "https://api.calendly.com/scheduled_events",
-      token = get_token(app_name = "calendly"),
-      user = user,
-      count = 100,
-      page_token = result_list$pagination$next_page_token
-    )
-    page <- page + 1
-    cummulative_pages[[page]] <- result_list$collection
+      result_list <-
+        calendly_get(
+          url = "https://api.calendly.com/scheduled_events",
+          token = get_token(app_name = "calendly"),
+          user = user,
+          count = 100,
+          page_token = result_list$pagination$next_page_token
+        )
+      page <- page + 1
+      cummulative_pages[[page]] <- result_list$collection
     }
 
     event_data <- dplyr::bind_rows(cummulative_pages)
   } else {
-
     # if less than 100 events were requested then we don't have any page stuff
     # to do, just return the first list
     event_data <- result_list$collection
