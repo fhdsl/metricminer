3ebe7fe6f16446633cb52d2216104323dc0df234
Candace Savonen <cansav09@gmail.com>
2023-11-13 09:59:32 -0500

    Gathering stats

--- R/github.R
+++ R/github.R
@@ -1,8 +1,9 @@
 # Extracting data from GitHub
 
-#' Get the GitHub User's info
+#' Wrapper function for GET requests from GitHub
 #' @description This is a function to get the GitHub user's info
-#' @param api_key You can provide the Personal Access Token key directly or this function will attempt to grab a PAT that was stored using the `authorize("github")` function
+#' @param token You can provide the Personal Access Token key directly or this function will attempt to grab a PAT that was stored using the `authorize("github")` function
+#' @param url What is the URL endpoint we are attempting to grab here?
 #' @return Information regarding a github account
 #' @importFrom utils menu installed.packages
 #' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
@@ -10,15 +11,9 @@
 #' @examples \dontrun{
 #'
 #' authorize("github")
-#' get_github_user()
+#' get_github()
 #' }
-get_github_user <- function(api_key) {
-  # Get auth token
-  token <- get_token(app_name = "github")
-
-  # Declare URL
-  url <- "https://api.github.com/user"
-
+get_github <- function(token, url) {
   # Github api get
   result <- httr::GET(
     url,
@@ -39,6 +34,80 @@ get_github_user <- function(api_key) {
 #' Get the GitHub User's info
 #' @description This is a function to get the GitHub user's info
 #' @param api_key You can provide the Personal Access Token key directly or this function will attempt to grab a PAT that was stored using the `authorize("github")` function
+#' @return Information regarding a github account
+#' @importFrom utils menu installed.packages
+#' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
+#' @export
+#' @examples \dontrun{
+#'
+#' authorize("github")
+#' get_github_user()
+#' }
+get_github_user <- function(api_key)
+
+  if (is.null(api_key)) {
+    # Get auth token
+    token <- get_token(app_name = "github")
+  } else {
+    token <- api_key
+  }
+
+    get_github(
+    url = "https://api.github.com/user",
+    token = token,
+  )
+}
+
+#' Get the repository info
+#' @description This is a function to get the GitHub user's info
+#' @param api_key You can provide the Personal Access Token key directly or this function will attempt to grab a PAT that was stored using the `authorize("github")` function
+#' @return Information regarding a github account
+#' @importFrom utils menu installed.packages
+#' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
+#' @export
+#' @examples \dontrun{
+#'
+#' authorize("github")
+#' get_github_repo()
+#' }
+get_github_repo <- function(api_key,  owner, repo) {
+
+  if (is.null(api_key)) {
+    # Get auth token
+    token <- get_token(app_name = "github")
+  } else {
+    token <- api_key
+  }
+  repo_activity <- gh::gh("GET /repos/{owner}/{repo}/activity",
+                          owner = owner,
+                          repo = repo,
+                          .token = token)
+
+
+
+  stars <- gh::gh("GET /repos/{owner}/{repo}/stargazers",
+                          owner = owner,
+                          repo = repo,
+                          .token = token)
+
+  forks <- gh::gh("GET /repos/{owner}/{repo}/forks",
+                  owner = owner,
+                  repo = repo,
+                  .token = token)
+
+  contributors <- gh::gh("GET /repos/{owner}/{repo}/contributors",
+                  owner = owner,
+                  repo = repo,
+                  .token = token)
+
+  return(list(repo_activity, stars, forks, contributors))
+
+}
+
+
+#' Get the metrics from GitHub on a repo
+#' @description This is a function to get the GitHub user's info
+#' @param api_key You can provide the Personal Access Token key directly or this function will attempt to grab a PAT that was stored using the `authorize("github")` function
 #' @param owner Who is the owner of this account? For example in the repository fhdsl/metricminer, fhdsl is the owner
 #' @param repo What is the repository name? For example in the repository fhdsl/metricminer, "metricminer" is the repo name.
 #' @return Information regarding a github account
@@ -49,7 +118,7 @@ get_github_user <- function(api_key) {
 #' authorize("github")
 #' get_github_user()
 #' }
-get_github_stats <- function(api_key, owner, repo) {
+get_github_metrics <- function(api_key, owner, repo) {
 
   if (is.null(api_key)) {
     # Get auth token
