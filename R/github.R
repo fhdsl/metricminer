3a6defbee4cceae8b6e61ca83e0191b623994bc8
Candace Savonen <cansav09@gmail.com>
2023-11-21 10:10:08 -0500

    Add bind_rows back now that it is working.

--- R/github.R
+++ R/github.R
@@ -195,6 +195,10 @@ get_repos_metrics <- function(owner = NULL, repo_names = NULL, token = NULL, dat
   # Keep names
   names(repo_metrics) <- repo_names
 
+  if (data_format == "dataframe") {
+    repo_metrics <- dplyr::bind_rows(all_repos_metrics)
+  }
+
   return(repo_metrics)
 }
 
