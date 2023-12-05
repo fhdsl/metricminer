c2bc462c8ff3e8ff5bc143e8a0d09175a8b62ad1
Candace Savonen <cansav09@gmail.com>
2023-12-05 12:56:15 -0500

    stash it

--- R/cran.R
+++ R/cran.R
@@ -1,14 +1,21 @@
 # CRAN
 
-# library("ggplot2")
-# library("dlstats")
+ library("ggplot2")
+ library("dlstats")
 
-# download_stats <- cran_stats(c("ottrpal", "githubr", "rgoogleclassroom"))
+ download_stats <- cran_stats(c("ottrpal", "metricminer", "conrad", "ari", "text2speech", "mario"))
 
-# if (!is.null(download_stats)) {
-#  print(head(download_stats))
-#  ggplot(download_stats, aes(end, downloads, group=package, color=package)) +
-#    geom_line() +
-#    geom_point() +
-#    scale_y_log10()
-# }
+ download_stats %>% dplyr::summarize(download_total = sum(downloads))
+
+ download_stats %>% dplyr::group_by(package) %>%
+   dplyr::summarize(download_total = sum(downloads))
+
+
+
+if (!is.null(download_stats)) {
+  print(head(download_stats))
+  ggplot(download_stats, aes(end, downloads, group=package, color=package)) +
+    geom_line() +
+    geom_point() +
+    scale_y_log10()
+ }
