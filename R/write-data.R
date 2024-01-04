3b9b0770727eed6837cfeafc60a1e6573b1dd60b
Candace Savonen <cansav09@gmail.com>
2024-01-04 09:36:33 -0500

    Basic structure

--- R/write-data.R
+++ R/write-data.R
@@ -1,23 +1,41 @@
-# library(metricminer)
-library(googlesheets4)
 
-# The data is publicly available so we don't need to authenticate
-gs4_deauth()
-# Publicly accessible Google Sheet
-sheet_url <- "https://docs.google.com/spreadsheets/d/13x8lD9SeuPGCs8SbtbRRK_q6o1V_rd3B07JuhebT-vk/edit?usp=sharing"
+#' Writes data to a Googlesheet
+#' @description This is a function to
+#' @param input
+#' @param gsheet
+#' @return The googlesheet URL where the data has been written
+#' @importFrom utils menu installed.packages
+#' @export
+#' @examples \dontrun{
+#'
+#' authorize("github")
+#' repo_list <- get_repo_list(owner = "fhdsl")
+#'
+#' write_to_gsheet(repo_list)
+#' }
+#'
 
-# Google Analytics----
-accounts <- get_ga_user()
-stats_list <- all_ga_metrics(account_id = accounts$id[5])
+write_to_gsheet <- function() {
 
-# Save the 3 elements of list as data frames
-metrics <- stats_list$metrics
-dimensions <- stats_list$dimensions
-link_clicks <- stats_list$link_clicks
+}
 
-# Write to Google Sheets
-metrics %>% sheet_write(ss = sheet_url, sheet = "metrics")
-dimensions %>% sheet_write(ss = sheet_url, sheet = "dimensions")
-link_clicks %>% sheet_write(ss = sheet_url, sheet = "link_clicks")
 
-# GitHub----
+
+#' Writes data to a tabular file
+#' @description
+#' @param input
+#' @param file_path
+#' @param table_type
+#' @return The file path where the data has been written
+#' @importFrom utils menu installed.packages
+#' @export
+#' @examples \dontrun{
+#'
+#' authorize("github")
+#' repo_list <- get_repo_list(owner = "fhdsl")
+#'
+#' write_to_table(repo_list)
+#' }
+write_to_table <- function() {
+
+}
