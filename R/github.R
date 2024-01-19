2fe99e2f40379d397cf75edb6d4781df21d3c230
Candace Savonen <cansav09@gmail.com>
2024-01-18 21:47:08 -0500

    Fix

--- R/github.R
+++ R/github.R
@@ -198,7 +198,7 @@ get_github_metrics <- function(repo, token = NULL, count = "all", data_format = 
 
   if (data_format == "dataframe") {
 
-    results <- summarize_repo_metrics(
+    results <- clean_repo_metrics(
       repo_name = paste0(c(owner, repo), collapse = "/"),
       repo_metric_list = results
     )
