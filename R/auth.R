362f7e4938d95d41bb3a0930ffdfb4745c0f5511
Howard Baek <50791792+howardbaek@users.noreply.github.com>
2024-02-14 12:21:49 -0800

    Titlecase roxygen documentation

--- R/auth.R
+++ R/auth.R
@@ -1,8 +1,8 @@
 #' Authorize R package to access endpoints
 #' @description This is a function to authorize the R package to access APIs interactively.
-#' @param app_name app would you like to authorize? Supported apps are 'google' 'calendly' and 'github'
+#' @param app_name App would you like to authorize? Supported apps are 'google' 'calendly' and 'github'
 #' @param cache Should the token be cached as an .httr-oauth file or API keys stored as global options?
-#' @param ... additional arguments to send to \code{\link{oauth2.0_token}}
+#' @param ... Additional arguments to send to \code{\link{oauth2.0_token}}
 #' @return API token saved to the environment or the cache so it can be grabbed by functions
 #' @importFrom utils menu installed.packages browseURL
 #' @importFrom httr oauth_app oauth_endpoints oauth2.0_token
