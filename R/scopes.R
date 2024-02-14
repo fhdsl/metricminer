a4cc8e93cb8cf8425fe95d31c4418fbcc9efdd3d
Candace Savonen <cansav09@gmail.com>
2024-02-14 15:20:28 -0500

    Reduce scopes

--- R/scopes.R
+++ R/scopes.R
@@ -4,11 +4,9 @@ find_scopes <- function(app_name) {
   ### Declare all the scopes
   scopes <- list(
     google = c(
-      "https://www.googleapis.com/auth/drive",
       "https://www.googleapis.com/auth/drive.file",
       "https://www.googleapis.com/auth/drive.readonly",
       "https://www.googleapis.com/auth/drive.metadata.readonly",
-      "https://www.googleapis.com/auth/forms.body",
       "https://www.googleapis.com/auth/forms.body.readonly",
       "https://www.googleapis.com/auth/forms.responses.readonly",
       "https://www.googleapis.com/auth/analytics.readonly",
