2abe889428e8072fd8f7ccdffb5a6d30ebb51279
Candace Savonen <cansav09@gmail.com>
2025-02-18 10:50:59 -0500

    Update scopes

--- R/scopes.R
+++ R/scopes.R
@@ -4,13 +4,13 @@ find_scopes <- function(app_name) {
   ### Declare all the scopes
   scopes <- list(
     google = c(
-      "https://www.googleapis.com/auth/drive.file",
       "https://www.googleapis.com/auth/drive.readonly",
       "https://www.googleapis.com/auth/drive.metadata.readonly",
-      "https://www.googleapis.com/auth/forms.body.readonly",
-      "https://www.googleapis.com/auth/forms.responses.readonly",
+      "https://www.googleapis.com/auth/drive.file",
+      "https://www.googleapis.com/auth/youtube.readonly",
       "https://www.googleapis.com/auth/analytics.readonly",
-      "https://www.googleapis.com/auth/youtube.readonly"
+      "https://www.googleapis.com/auth/forms.responses.readonly",
+      "https://www.googleapis.com/auth/forms.body.readonly"
     ),
     github = c("repo")
   )
